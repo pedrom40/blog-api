d93a09dec598ed2bf59198118e1efd5d17b84194
pedrom40 <pedro.morin1980@gmail.com>
2017-10-08 12:00:22 -0500

    setup get blog posts route

--- server.js
+++ server.js
@@ -6,11 +6,23 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 // setup app
+const {BlogPosts} = require('./models');
 const app = express();
 
 
 // log the http layer
 app.use(morgan('common'));
+
+
+// manually create some blog posts
+BlogPosts.create('Creating Your 1st API', 'Once you get the hang of it, you will never look back.', 'Hello World');
+BlogPosts.create('Understanding Express.js', 'If you have any experience with JavaScript, then you already have a head start.', 'Hello World');
+
+
+// first route
+app.get('/blog-posts', (req, res) => {
+  res.json(BlogPosts.get());
+});
 
 
 // setup server
